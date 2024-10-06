c53996d8bcc9f79cd1ded5deea33b8424b915498
Kuleen Sasse <cometku@gmail.com>
2024-10-06 18:51:47 -0400

    debugged processing pipeline

--- processing_pipeline.sh
+++ processing_pipeline.sh
@@ -1,11 +1,8 @@
 INPUT_FOLDER="$1"
 OUTPUT_FOLDER="$2"
 
-mkdir -p $OUTPUT_FOLDER/conll
 mkdir -p $OUTPUT_FOLDER/labelled_conll
 
-python pipeline/txt_to_conll.py -i $INPUT_FOLDER -o $OUTPUT_FOLDER/conll
-
-python -m src.ner.predict.py -c $OUTPUT_FOLDER/labelled_conll -m biobert --files $OUTPUT_FOLDER/conll/*
+python -m src.ner.pipeline_predict -c $OUTPUT_FOLDER/labelled_conll -m biobert --files $INPUT_FOLDER/*
 
 python -m src.normalization_models.krissbert.predict --files $OUTPUT_FOLDER/labelled_conll/* --encoded_files prototypes/embeddings --entity_list_ids prototypes/cuis
