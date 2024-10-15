5ef2ed97f246e3e252f2f809d136c5a202eb9d18
Kuleen Sasse <cometku@gmail.com>
2024-10-14 22:12:57 -0400

    debugging the processing pipeline script with headerbash

--- processing_pipeline.sh
+++ processing_pipeline.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 INPUT_FOLDER="$1"
 OUTPUT_FOLDER="$2"
 
 mkdir -p $OUTPUT_FOLDER/labelled_conll
+mkdir -p $OUTPUT_FOLDER/ann_files
 
 python -m src.ner.pipeline_predict -c $OUTPUT_FOLDER/labelled_conll -m biobert --files $INPUT_FOLDER/*
 
-python -m src.normalization_models.krissbert.predict --files $OUTPUT_FOLDER/labelled_conll/* --encoded_files prototypes/embeddings --entity_list_ids prototypes/cuis
+python -m src.normalization_models.krissbert.predict --output_dir $OUTPUT_FOLDER/ann_files --files $OUTPUT_FOLDER/labelled_conll/* --encoded_files prototypes/embeddings --entity_list_ids prototypes/cuis
