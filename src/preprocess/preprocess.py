1fafd2aa79e03a0b943b939b445ea0615e6992a2
Kuleen Sasse <cometku@gmail.com>
2025-01-28 22:54:55 -0600

    better preprocess script

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,5 +1,5 @@
 import argparse
-import tomllib
+import tomli
 
 from src.preprocess.generate_semeval_data import GenerateSemevalData
 
@@ -8,16 +8,14 @@ def main(args):
 
     config_path = args.config
     with open(config_path, "rb") as f:
-        config = tomllib.load(f)
+        config = tomli.load(f)
 
     preprocess_config = config['preprocess']
 
     semeval_data = preprocess_config['semeval_data']
     output_data = preprocess_config['semeval_output_data']
 
-    preprocessor = GenerateSemevalData(semeval_data, output_data)
-
-    preprocessor.generate()
+    GenerateSemevalData(semeval_data, output_data).generate()
 
 
 if __name__ == "__main__":
