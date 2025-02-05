d53869a13e256737012f5176dd9f3b78b47e6af7
Kuleen Sasse <cometku@gmail.com>
2025-02-05 10:34:40 -0600

    updated generate

--- src/generate/generate.py
+++ src/generate/generate.py
@@ -4,6 +4,8 @@ import tomli
 
 from llama import LLaMaGenerate
 from input_data import InputData
+from synonym_replacement import SynonymReplacement
+from template import Templating
 
 def main(args):
 
@@ -15,25 +17,42 @@ def main(args):
 
     output_folder = generation_config['output_folder']
 
-    input_file = generation_config['input_file']
+    for synth_data in generation_config['synthetic_data']:
 
-    model_dir = generation_config['model_dir']
+        if synth_data == "llm":
+            input_file = generation_config["llm"]['input_file']
 
-    generation_params = generation_config['generation_params']
+            model_dir = generation_config["llm"]['model_dir']
 
-    decoder_name = generation_params["model_name"].replace("/", "")
+            generation_params = generation_config["llm"]['generation_params']
 
-    os.makedirs(output_folder, exist_ok=True)
+            decoder_name = generation_params["model_name"].replace("/", "")
 
-    if not os.path.exists(input_file):
-        input_data = InputData(input_file)
+            os.makedirs(output_folder, exist_ok=True)
 
-        input_data.generate()
+            if not os.path.exists(input_file):
+                input_data = InputData(input_file)
 
-    llama_umls_data = LLaMaGenerate(input_file, os.path.join(output_folder, f"generation_data_{decoder_name}.csv"), model_dir, generation_params)
+                input_data.generate()
+            
+            llama_umls_data = LLaMaGenerate(input_file, os.path.join(output_folder, f"generation_data_{decoder_name}.csv"), model_dir, generation_params)
 
-    llama_umls_data.generate()
+            llama_umls_data.generate()
 
+        elif synth_data == "template":
+            template_data = Templating(os.path.join(output_folder, f"template_data.csv"))
+
+            template_data.generate()
+        
+        elif synth_data == "synonym":
+
+            dataset = generation_config["synonym"]["dataset"]
+
+            semeval_folder = generation_config["synonym"]["semeval_folder"]
+
+            syn_data = SynonymReplacement(dataset, os.path.join(output_folder, f"syn_replacement_data.csv"), semeval_folder)
+
+            syn_data.generate()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
