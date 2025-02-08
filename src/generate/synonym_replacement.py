d7188233929ce5ac2e034bfa954f3a283111c860
Kuleen Sasse <cometku@gmail.com>
2025-02-07 22:14:11 -0600

    debugged synonym and templates

--- src/generate/synonym_replacement.py
+++ src/generate/synonym_replacement.py
@@ -12,54 +12,117 @@ import mysql.connector as mariadb
 
 from datasets import load_dataset
 
+from tqdm import tqdm
+
+
+CUI_FIX = {"C1883552":"C0004093",
+"C0021311":"C0009450",
+"C0342243":"C0020456",
+"C0242362":"C0021818",
+"C0228070":"C0027757",
+"C1704437":"C0035220",
+"C2919362":"C0376670",
+"C0050252":"C0377562",
+"C0339663":"C0549122",
+"C2712054":"C0743330",
+"C0557875":"C0849970",
+"C0032305":"C1535939",
+"C0337197":"C1579887",
+"C0005001":"C1704272",
+"C1960564":"C2020625",
+"C0432747":"C2108211",
+"C0151706":"C2979982",
+"C0175677":"C3263723",
+"C0232280":"C4049827",
+"C1883552.":"C0004093",
+"C00018946":"C0018946",
+"C0040053.":"C0040053",
+"CUi-less":"cui-less",
+"CUI-less":"cui-less",
+"Cui-less":"cui-less",
+"C0003878":"C0003873",
+"C002076":"C0020676",
+"C00240066":"C0240066",
+"C023529":"C0235329",
+"C03203358":"C3203358"}
 
 class SynonymReplacement:
 
-    def __init__(self, dataset: str, output_file: str, input_folder: str = None) -> None:
+    def __init__(self, dataset: str, output_file: str, semeval_folder: str = None, mrconso_file: str = None, history_file: str = None) -> None:
         
         self.dataset = dataset
 
         self.output_file = output_file
 
-        self.input_folder = input_folder
+        self.semeval_folder = semeval_folder
 
-        self.query = "SELECT STR FROM MRCONSO WHERE CUI='{cui}' AND LAT='ENG AND ISPREF = 'Y';"
+        self.history_file = history_file
 
-        user = os.environ["UMLS_USER"]
-        pwd = os.environ["UMLS_PWD"]
-        database = os.environ["UMLS_DATABASE_NAME"]
-        ip = os.environ["UMLS_IP"]
+        if self.history_file is not None:
+            self.history_df = pd.read_csv(history_file, sep="|", header=None)
+        else:
+            self.history_df = None
 
-        mariadb_connection = mariadb.connect(
-            user=user, password=pwd, database=database, host=ip
-        )
+        if mrconso_file is not None:
+            mrconso_file = pd.read_csv(mrconso_file, sep="|", header=None)
 
-        df_omim = pd.read_sql(
-            "SELECT CUI, SAB, CODE, STR FROM MRCONSO WHERE SAB LIKE '%OMIM%'",
-            con=mariadb_connection,
-        )
-        df_msh = pd.read_sql(
-            "SELECT CUI, SAB, CODE, STR FROM MRCONSO WHERE SAB LIKE '%MSH%'",
-            con=mariadb_connection,
-        )
+            df = mrconso_file[[0,11,13,14]]
 
-        df_omim.columns = ["CUI", "OMIM_SAB", "OMIM_CODE", "OMIM_STR"]
-        df_msh.columns = ["CUI", "MSH_SAB", "MSH_CODE", "MSH_STR"]
+        else:
 
-        self.omim_to_cui = dict(zip(list(df_omim["OMIM_CODE"]), list(df_omim["CUI"])))
+            user = os.environ["UMLS_USER"]
+            pwd = os.environ["UMLS_PWD"]
+            database = os.environ["UMLS_DATABASE_NAME"]
+            ip = os.environ["UMLS_IP"]
 
-        self.mesh_to_cui = dict(zip(list(df_msh["MSH_CODE"]), list(df_msh["CUI"])))
+            mariadb_connection = mariadb.connect(
+                user=user, password=pwd, database=database, host=ip
+            )
+
+            df = pd.read_sql(
+                "SELECT CUI, SAB, CODE, STR FROM MRCONSO",
+                con=mariadb_connection,
+            )
+
+        df.columns = ["CUI", "SAB", "CODE", "STR"]
+
+        self.code_to_cui = dict(zip(list(df["CODE"]), list(df["CUI"])))
+
+        self.cui_to_strings = dict()
+
+        for _, row in df.iterrows():
+            cui_row = row["CUI"]
+            cui_str = row["STR"]
+
+            if cui_row not in self.cui_to_strings:
+
+                str_set = set()
+
+                str_set.add(cui_str)
+            
+            else:
+                str_set: set = self.cui_to_strings[cui_row]
+
+                str_set.add(cui_str)
+            
+            self.cui_to_strings[cui_row] = str_set
     
-    def _split_offsets(string_input: str) -> np.ndarray:
+    def _split_offsets(self, string_input: str) -> np.ndarray:
         return np.array([x.split('-') for x in string_input.split(',')]).astype(int)
     
     def _get_syn(self, cui: str) -> List[str]:
+        if cui in self.cui_to_strings:
+            return list(self.cui_to_strings[cui])
+        elif self.history_df is not None:
+            data = list(self.history_df[self.history_df[0] == cui][14])
 
-        syns = pd.read_sql(self.query.format(cui=cui))
+            if len(data) == 0:
+                raise ValueError(f"{cui} does not exist in history")
+            else:
+                return data
 
-        syns.columns = ["STR"]
-
-        return list(syns["STR"])
+        else:
+            raise ValueError(f"{cui} does not exist in cui to string")
 
     def _syn_replace(self, text: str, start: int, end: int, syns: List[str]) -> List[str]:
 
@@ -67,18 +130,25 @@ class SynonymReplacement:
 
         for syn in syns:
 
-            new_text = text = text[:start] + f' <1CUI> ' + syn + f' <1CUI> ' + text[end:]
+            new_text = replaced_texts[0][:start] + f' <1CUI> ' + syn + f' </1CUI> ' + replaced_texts[0][end:]
             
             replaced_texts.append(new_text)
 
         return replaced_texts
     
-    def _map_to_cui(item, omim_to_cui, mesh_to_cui):
-        if item in omim_to_cui:
-            return omim_to_cui[item]
-        elif item in mesh_to_cui:
-            return mesh_to_cui[item]
-        return item
+    def _map_to_cui(self, item):
+        if item in self.code_to_cui:
+            return self.code_to_cui[item]
+        elif self.history_df is not None:
+            data = self.history_df[self.history_df[10] == item][0]
+
+            if len(data) == 0:
+                raise ValueError(f"{item} does not exist in history")
+            else:
+                return data.iloc[0]
+
+        else:
+            raise ValueError(f"{item} does not exist in omim/mesh to cui")
     
     def generate(self):
         if self.dataset == "bc5cdr":
@@ -86,7 +156,7 @@ class SynonymReplacement:
         elif self.dataset == "ncbi":
             self._process_ncbi()
         else:
-            self._process_semeval(self.input_folder)
+            self._process_semeval(self.semeval_folder)
     
     def _process_bc5cdr(self):
 
@@ -94,13 +164,15 @@ class SynonymReplacement:
 
         passages = dataset["train"]["passages"]
 
+        number_of_cuis = 0
+
         with open(self.output_file, "w") as f:
 
             csv_file = csv.writer(f)
 
             csv_file.writerow(["cui", "matched_output"])
 
-            for passage in passages:
+            for passage in tqdm(passages):
                 text = passage[0]["text"]+ " " + passage[1]["text"]
 
                 entities = passage[0]["entities"]
@@ -114,7 +186,7 @@ class SynonymReplacement:
 
                         for offset, normalized_item in zip(offsets, normalized):
 
-                            mapped_cui = self._map_to_cui(normalized_item["db_id"], self.omim_to_cui, self.mesh_to_cui)
+                            mapped_cui = self._map_to_cui(normalized_item["db_id"])
 
                             synoynms = self._get_syn(mapped_cui)
 
@@ -122,7 +194,7 @@ class SynonymReplacement:
 
                             for syn_replaced_text in syn_replaced_texts:
                                 csv_file.writerow([mapped_cui, syn_replaced_text])
-
+            
     def _process_ncbi(self):
 
         dataset = load_dataset("bigbio/ncbi_disease")
@@ -139,19 +211,24 @@ class SynonymReplacement:
 
             csv_file.writerow(["cui", "matched_output"]) 
 
-            for title, abstract, list_of_mentions in zip(titles, abstracts, mentions):
+            for title, abstract, list_of_mentions in tqdm(zip(titles, abstracts, mentions)):
                 text = title + " " + abstract
 
                 for mention in list_of_mentions:
-                    concept_ids_split = mention["concept_id"].split("|")
+                    if "+" in mention["concept_id"]:
+                        concept_ids_split = mention["concept_id"].split("+")
+                    else:
+                        concept_ids_split = mention["concept_id"].split("|")
 
                     offset = mention["offsets"]
 
                     for concept_id_split in concept_ids_split:
                         if "OMIM" in concept_id_split:
                             concept_id_split = concept_id_split.replace("OMIM:", "")
+                        
+                        concept_id_split = concept_id_split.strip()
 
-                        mapped_cui = self._map_to_cui(concept_id_split, self.omim_to_cui, self.mesh_to_cui)
+                        mapped_cui = self._map_to_cui(concept_id_split)
 
                         synoynms = self._get_syn(mapped_cui)
 
@@ -170,11 +247,14 @@ class SynonymReplacement:
 
             csv_file.writerow(["cui", "matched_output"]) 
 
-            for filename in list_of_pipe_files:
+            for filename in tqdm(list_of_pipe_files):
                 df = pd.read_csv(filename, sep='|', header=None, on_bad_lines='warn')
-                df[2] = df[2].fillna('CUI-less')
 
-                df = df[df[2] != "CUI-less"]
+                df[2] = df[2].fillna('cui-less')
+
+                df[2] = df[2].replace(CUI_FIX)
+
+                df = df[df[2] != "cui-less"]
 
                 if len(df) != 0:
 
--- src/generate/template.py
+++ src/generate/template.py
@@ -1,17 +1,22 @@
+from pathlib import Path 
+
 import csv
 
 import os
 
 import mysql.connector
 import pandas as pd 
+import polars
+
 
 from tqdm import tqdm
 
 class Templating:
 
-    def __init__(self, output_file: str) -> None:
+    def __init__(self, output_file: str, umls_folder: str = None) -> None:
 
         self.output_file = output_file
+        self.umls_folder = umls_folder
         self.user = os.environ['UMLS_USER']
         self.pwd = os.environ['UMLS_PWD']
         self.database = os.environ['UMLS_DATABASE_NAME']
@@ -24,16 +29,41 @@ Plan: Proceed with necessary assessments and initiate appropriate care as indica
 
     def generate(self):
 
-        cnx = mysql.connector.connect(user=self.user, password=self.pwd, database=self.database, host=self.ip)
+        if self.umls_folder is not None:
 
-        query = ("SELECT MRCONSO.CUI, MRCONSO.STR, MRDEF.DEF FROM MRCONSO \
-        JOIN MRSTY ON (MRCONSO.CUI = MRSTY.CUI) \
-        JOIN MRDEF ON (MRCONSO.CUI = MRDEF.CUI) \
-        WHERE LAT='ENG' AND ISPREF='Y' \
-        AND TUI IN ('T047','T020','T190','T049','T019','T050','T033','T037','T048','T191','T046','T184') \
-        GROUP BY MRCONSO.CUI ORDER BY MRCONSO.CUI;") 
+            cnx = mysql.connector.connect(user=self.user, password=self.pwd, database=self.database, host=self.ip)
 
-        df = pd.read_sql(query, con=cnx)
+            query = ("SELECT MRCONSO.CUI, MRCONSO.STR, MRDEF.DEF FROM MRCONSO \
+            JOIN MRSTY ON (MRCONSO.CUI = MRSTY.CUI) \
+            JOIN MRDEF ON (MRCONSO.CUI = MRDEF.CUI) \
+            WHERE LAT='ENG' AND ISPREF='Y' \
+            AND TUI IN ('T047','T020','T190','T049','T019','T050','T033','T037','T048','T191','T046','T184') \
+            GROUP BY MRCONSO.CUI ORDER BY MRCONSO.CUI;") 
+
+            df = pd.read_sql(query, con=cnx)
+        
+        else:
+            root = Path(self.umls_folder)
+
+            mrconso = root / "MRCONSO.RRF"
+
+            mrsty = root / "MRSTY.RRF"
+
+            MRCONSO = polars.read_csv(mrconso, separator="|", has_header=False, encoding="utf8", quote_char=None)
+
+            MRSTY = polars.read_csv(mrsty, separator="|", has_header=False, encoding="utf8", quote_char=None)
+
+            MRCONSO.columns = ["CUI", "LAT", "TS", "LUI", "STT", "SUI", "ISPREF", "AUI", "SAUI", "SCUI", "SDUI", "SAB", "TTY", "CODE", "STR", "SRL", "SUPPRESS", "CVF", "BLANK"]
+
+            MRSTY.columns = ["CUI", "TUI", "STN", "STY", "ATUI", "CVF", "BLANK"]
+
+            MRSTY_FILTER = MRSTY.filter(polars.col('TUI').str.contains_any(['T047','T020','T190','T049','T019','T050','T033','T037','T048','T191','T046','T184']))[["CUI", "TUI"]]
+
+            MRCONSO_FILTER = MRCONSO.filter((polars.col("LAT") == "ENG") & (polars.col("ISPREF") == "Y"))[["CUI", "STR"]]
+
+            MRCONSO_JOIN_MRSTY = MRCONSO_FILTER.join(MRSTY_FILTER, left_on="CUI", right_on="CUI", how="inner")
+
+            df = MRCONSO_JOIN_MRSTY.unique("CUI")
 
         with open(self.output_file, "w") as f:
             csv_file = csv.writer(f)
